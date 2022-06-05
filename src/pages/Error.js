24e040f0f318328ccc8d2fcffe0def3d320e4166
gandor999 <geodorruales@gmail.com>
2022-06-05 22:28:02 +0800

    refactor: refactor Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 
-export default function ErrorPage(){
-	return (
-		<Row>
-			<Col className="p-5">
-				<h1>Page Not Found</h1>
-				<p>Go back to the <Link to="/">homepage</Link></p> 
-			</Col>
-		</Row>
-	)
+export default function ErrorPage() {
+  return (
+    <Row>
+      <Col className="p-5">
+        <h1>Page Not Found</h1>
+        <p>
+          Go back to the <Link to="/">homepage</Link>
+        </p>
+      </Col>
+    </Row>
+  );
 }
